beb9226e75099349facb2da5b235cf41128c3fc0
manpervel2 <manpervel2@alum.us.es>
2025-04-07 15:57:30 +0200

    fix: fix a route

--- eventbride/src/pages/index.js
+++ eventbride/src/pages/index.js
@@ -28,7 +28,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Inicio"
+            to="/docs/intro"
           >
             Acceso a la documentación
           </Link>
